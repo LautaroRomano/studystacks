cccdab49a476ff1f817b63d2b137deb126a4c2bc
Marce952 <101135405+Marce952@users.noreply.github.com>
2023-04-04 23:32:24 -0300

    Update index.js

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -3,7 +3,33 @@ import { pool } from "../../../config/database";
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
-      const [result] = await pool.query("SELECT * FROM users");
-      return res.status(200).json(result);
+      return await get(req, res)
+    case "POST":
+      return await post(req, res)
+
   }
 }
+
+  const get = async () => {
+    try {
+      const [result] = await pool.query(`SELECT * FROM Users`);
+      return res.status(200).json(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const post = async () => {
+    const { username, email, password, last_login_date } = req.body
+    try {
+      const [result] = await pool.query(`INSERT INTO Users SET ?`,{
+          username,
+          email,
+          password,
+          last_login_date
+      });
+      return res.status(200).json(result);
+    } catch (error) {
+      console.log(error);
+    }
+  }
