45c6977a527d6b18f44eadec1f34ba913a5f165f
LautaroRomano <71612453+LautaroRomano@users.noreply.github.com>
2023-04-17 19:50:27 -0300

    funcion hash para contrasenias

--- pages/api/login/isloggedin/[email].js
+++ pages/api/login/isloggedin/[email].js
@@ -4,8 +4,6 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "GET":
       return await get(req, res);
-    case "POST":
-      return await post(req, res);
   }
 }
 
@@ -21,22 +19,3 @@ const get = async (req, res) => {
   }
 };
 
-const post = async (req, res) => {
-  const {
-    community_name,
-    community_description,
-    creation_date,
-    creator_user_id,
-  } = req.body;
-  try {
-    const [result] = await pool.query(`INSERT INTO communities SET ?`, {
-      community_name,
-      community_description,
-      creation_date,
-      creator_user_id,
-    });
-    return res.status(200).json(result);
-  } catch (error) {
-    console.log(error);
-  }
-};
