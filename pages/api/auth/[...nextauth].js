bc10727376d0e23fe4843a95fde669016efdfcf0
Emiliano Alejandro <alejandroemiliano21@gmail.com>
2023-04-19 12:53:48 -0300

    Archvio Login.js
    Toma si iniciaste sesion, valida cuando quieres crear un newpost valida si tenes sesion inciada, si no lo tenes te manda a que inicies sesion.

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,8 +7,12 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+      scope: 'profile'
     }),
   ],
+   pages:{ 
+    signIn:'/login'
+   },
   // url: 'http://localhost:3000',
   url: process.env.NEXTAUTH_URL,
   secret: process.env.NEXTAUTH_SECRET,
---
+++ pages/login.js
@@ -0,0 +1,33 @@
+import { Button, Flex } from "@chakra-ui/react";
+import {useState, useEffect} from "react"
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+export default function Login() {
+
+  const [error, setError] = useState(null)
+  const router = useRouter();
+
+  useEffect(()=>{
+    sessionStorage.setItem("callbackUrl", router.asPath);
+  }, [router.asPath])
+
+  const handleSignIn = async () => {
+    // signIn("google");
+  // const result = await signIn('google',  { callbackUrl: "/newpost" });
+  //
+  const callbackUrl = sessionStorage.getItem("callbackUrl") || "/";
+  const result = await signIn('google',  { callbackUrl});
+  
+  if(result?.error){
+      setError(result.error)
+    }
+  };
+
+  return (
+    <Flex align="center" justify="center" h="100vh" bg="gray.100">
+      <Button onClick={handleSignIn}>Sign in with Google</Button>  
+      {error && <p>{error}</p>}
+    </Flex>
+  );
+}
