191317ed3fcfb06390beaf0c1ba3a0bd4f42b3f6
LautaroRomano <71612453+LautaroRomano@users.noreply.github.com>
2023-04-21 21:08:21 -0300

    universidad, carrera y comision dentro de una comision
    Se cambio la base de datos!

--- components/communities/CreateCommunityView.jsx
+++ components/communities/CreateCommunityView.jsx
@@ -1,10 +1,23 @@
-import { Flex, Text, Textarea, Input, Button, Link } from "@chakra-ui/react";
+import {
+  Flex,
+  Text,
+  Textarea,
+  Input,
+  Button,
+  Link,
+  Select,
+  FormControl,
+  Switch,
+  FormLabel,
+} from "@chakra-ui/react";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 export default function CreateCommunityView({ session, userLoggin }) {
   const [communityData, setCommunityData] = useState({});
   const [postSucces, setPostSucces] = useState(false);
+  const [universityOn, setUniversityOn] = useState(false);
+  const [universities, setUniversities] = useState({});
 
   const handleChangeData = ({ target }) => {
     setCommunityData((data) => ({ ...data, [target.name]: target.value }));
@@ -15,11 +28,20 @@ export default function CreateCommunityView({ session, userLoggin }) {
       community_name: communityData.community_name,
       community_description: communityData.community_description,
       creator_user_id: userLoggin.user_id,
+      university_id: communityData.university_id,
+      career_id: communityData.career_id,
+      commission_id: communityData.commission_id,
     };
     axios.post("/api/communities", data).then(({ data }) => {
       if (data.succes) setPostSucces(true);
     });
   };
+
+  useEffect(() => {
+    axios.get("/api/universities").then(({ data }) => {
+      setUniversities(data);
+    });
+  }, []);
 
   return (
     <Flex
@@ -138,6 +160,115 @@ export default function CreateCommunityView({ session, userLoggin }) {
               onChange={handleChangeData}
             ></Textarea>
 
+            <FormControl
+              letterSpacing=".4px"
+              fontSize="14px"
+              fontWeight="500"
+              color="primaryGray.500"
+              marginTop="10px"
+              display={"flex"}
+              alignItems={"center"}
+            >
+              <FormLabel htmlFor="universityOn" mb="0">
+                Tu comunidad pertenece a una universidad?
+              </FormLabel>
+              <Switch
+                id="universityOn"
+                mt={"5px"}
+                value={universityOn}
+                onChange={({ target }) =>
+                  setUniversityOn(
+                    target.value === "false"
+                      ? true
+                      : target.value === "true"
+                      ? false
+                      : null
+                  )
+                }
+              />
+            </FormControl>
+            {universityOn && (
+              <>
+                <Text
+                  letterSpacing=".4px"
+                  fontSize="14px"
+                  fontWeight="500"
+                  color="primaryGray.500"
+                  marginTop="10px"
+                >
+                  Universidad:
+                </Text>
+                <Select
+                  value={communityData.university_id}
+                  name="university_id"
+                  onChange={handleChangeData}
+                  color={"primaryGray.500"}
+                >
+                  <option value={0}>{"Selecciona una universidad"}</option>
+                  {universities.universities &&
+                    universities.universities.map((uni) => (
+                      <option value={uni.university_id} key={uni.university_id}>
+                        {uni.university_name}
+                      </option>
+                    ))}
+                </Select>
+                <Text
+                  letterSpacing=".4px"
+                  fontSize="14px"
+                  fontWeight="500"
+                  color="primaryGray.500"
+                  marginTop="10px"
+                >
+                  Carrera:
+                </Text>
+                <Select
+                  color={"primaryGray.500"}
+                  value={communityData.career_id}
+                  name="career_id"
+                  onChange={handleChangeData}
+                >
+                  <option value={0}>{"Selecciona una carrera"}</option>
+                  {universities.careers &&
+                    universities.careers
+                      .filter(
+                        (c) => c.university_id == communityData.university_id
+                      )
+                      .map((career) => (
+                        <option value={career.career_id} key={career.career_id}>
+                          {career.career_name}
+                        </option>
+                      ))}
+                </Select>
+                <Text
+                  letterSpacing=".4px"
+                  fontSize="14px"
+                  fontWeight="500"
+                  color="primaryGray.500"
+                  marginTop="10px"
+                >
+                  Comision/Curso:
+                </Text>
+                <Select
+                  color={"primaryGray.500"}
+                  value={communityData.commission_id}
+                  name="commission_id"
+                  onChange={handleChangeData}
+                >
+                  <option value={0}>{"Selecciona una comision/curso"}</option>
+                  {universities.commissions &&
+                    universities.commissions
+                      .filter((c) => c.career_id == communityData.career_id)
+                      .map((com) => (
+                        <option
+                          value={com.commission_id}
+                          key={com.commission_id}
+                        >
+                          {com.commission_name}
+                        </option>
+                      ))}
+                </Select>
+              </>
+            )}
           </Flex>
           <Flex mt="20px" alignItems="center" justifyContent={"end"}>
             <Button
